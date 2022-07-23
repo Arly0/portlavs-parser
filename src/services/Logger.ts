c792d474ca5b40e262cb079f8da9a38719eb438c
Nazar <mrsmileball@gmail.com>
2022-07-23 12:34:48 +0200

    rework Logger

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -1,13 +1,42 @@
 import { ErrorTypes } from "../interface/LoggerInterface"
+const fs = require("fs");
 
-export const Logger = (msg:any, type: ErrorTypes = ErrorTypes.Error) => {
-  if (process.env.APP_DEBUG) {
-    if (type === ErrorTypes.Error) {
-      console.error(msg);
-    } else if (type === ErrorTypes.Warning) {
-      console.warn(msg);
+export class Logger
+{
+  _path: string;
+  _log_to_console: Boolean;
+  _new_file: Boolean;
+
+  constructor(logToConsole = false, newFile = false)
+  {
+    this._path = __dirname + '/../logs/error.log';
+    this._log_to_console = logToConsole;
+    this._new_file = newFile;
+  }
+
+
+  /**
+   * Writing log to file/console
+   * @param content string - text
+   */
+  writeLog (content: string)
+  {
+    if (process.env.APP_DEBUG && this._log_to_console) {
+      console.log(content);
+    }
+
+    if (this._new_file) {
+      fs.writeFile(this._path, content, function (err:any) {
+      if (process.env.APP_DEBUG && err) {
+        console.error(err);
+        }
+      });
     } else {
-      console.info(msg);
+      fs.appendFile(this._path, content + "\n", function (err:any) {
+        if (process.env.APP_DEBUG && err) {
+          console.error(err);
+        }
+      });
     }
   }
 }
